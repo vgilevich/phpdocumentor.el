466731234d5f3e8dff199daf7efe5ed70180dfc9
Moises Gallego <moisesgallego@gmail.com>
2012-11-22 13:47:06 +0100

    Fix the function name

--- phpdocumentor.el
+++ phpdocumentor.el
@@ -17,6 +17,27 @@
   (message "PHPDocumentor block created")
 )
 
+
+(defun php-create-setter ()
+  "create-the-setter-for-a-variable"
+  (interactive) 
+  (search-backward "$")
+  (setq method-name (phpdoc-get-method-description))
+  (setq params (phpdoc-get-params))
+  (phpdoc-block-position)
+  (setq inicio (point))
+  (setq init-block-point (point))
+  (phpdoc-start-line)
+  (phpdoc-new-line method-name)
+  (phpdoc-new-line)
+  (phpdoc-insert-params params)
+  (phpdoc-end-line)
+  (indent-region inicio (point))
+  (goto-char init-block-point)
+  (message "PHPDocumentor block created")
+)
+
+
 (defun phpdoc-block-position ()
   (previous-line)(beginning-of-line)(newline)
 )
@@ -36,11 +57,20 @@
 )
 
 (defun phpdoc-get-method-description ()
+  (right-word)
   (search-forward " ")
   (setq init-word (point))
   (right-word)
   (buffer-substring-no-properties init-word (point))
 )
+
+(defun phpdoc-get-variable-name ()
+  (search-forward " ")
+  (setq init-word (point))
+  (right-word)
+  (buffer-substring-no-properties init-word (point))
+)
+
 
 (defun phpdoc-insert-params (param-list)
   (if (> (length param-list) 0)
@@ -60,3 +90,5 @@
   (setq param-list (split-string (replace-regexp-in-string " " "" (replace-regexp-in-string ")" "" params)) ","))
 )
 
+(fset 'php-create-setter
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217848 115 97 backspace 101 97 114 tab 98 97 tab return 36 return right 67108896 C-right 134217847 134217790 134217848 115 101 97 114 tab 98 97 tab return 125 return return up return tab 112 117 98 99 108 105 backspace backspace backspace 108 105 99 32 102 117 110 99 105 backspace backspace 99 116 105 111 110 32 115 101 116 25 C-left 21 51 right 67108896 right 134217848 99 97 112 105 116 97 108 105 tab 45 114 101 103 tab return 5 40 36 25 41 32 123 return tab 36 116 104 105 115 45 62 25 32 61 32 36 25 59 return 125 return] 0 "%d")) arg)))
